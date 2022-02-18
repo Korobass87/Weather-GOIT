6d6a20de3ed450b6d79dd89e314de3797471fc46
Vyacheslav <dikey677@gmail.com>
2022-02-18 21:40:21 +0300

    bagfix

--- src/favorits.js
+++ src/favorits.js
@@ -11,29 +11,33 @@
 //   renderFavor.insertAdjacentHTML('beforeend', markup);
 // }
 
-import debounce from 'lodash.debounce';
+// import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 800;
 const CURRENT_CITY_NAME = 'current-city-name';
+let formData = [];
 
-const boxForm = document.querySelector('.find-form');
-const favorBtn = document.querySelector('.add-favor-btn');
+const form = document.querySelector('.find-form');
+const input = document.querySelector('.input-value');
+const button = document.querySelector('.add-favor-btn');
 
-boxForm.addEventListener('input', debounce(onInputCity, DEBOUNCE_DELAY));
-favorBtn.addEventListener('click', onClickAddFavor);
-
-function onInputCity(e) {
-  const currentCityName = e.target.value.trim();
-  console.log('SET CITY NAME ON LOCAL STORAGE:', currentCityName);
-  localStorage.setItem(CURRENT_CITY_NAME, currentCityName);
-}
+button.addEventListener('click', onClickAddFavor);
 
 function onClickAddFavor(e) {
   e.preventDefault();
 
-  // Backend data
-  return onCityLocalFetch(CURRENT_CITY_NAME);
+  let cityName = input.value.trim();
+  console.log('SET CITY NAME TO LOCAL STORAGE:', cityName);
+
+  if (cityName.length > 0 && !formData.includes(cityName) && cityName !== Number) {
+    formData.push(cityName);
+    console.log(formData);
+    localStorage.setItem(CURRENT_CITY_NAME, formData);
+  }
+
+  onCityLocalFetch(CURRENT_CITY_NAME);
+  return form.reset();
 }
 
 // Executive code
@@ -41,18 +45,20 @@ function onCityLocalFetch(name) {
   setTimeout(() => {
     const getCityName = localStorage.getItem(name);
 
+    const jsonPrs = JSON.parse(getCityName);
+    console.log(jsonPrs);
+
     return fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${getCityName}&units=metric&lang=ru,en&appid=daa3c03c1253f276d26e4e127c34d058`,
     ).then(response => {
       if (!response.ok) {
-        Notify.failure(`Sorry! This city doesn't exist. Enter valid city details`);
+        Notify.failure(`Sorry! This city doesn't exist. Enter valid city name`);
         throw new Error(response.status);
       }
 
       console.log('GET CITY NAME FROM LOCAL STORAGE:', getCityName);
       console.log(response);
 
-      localStorage.removeItem(name);
       return response.json();
     });
   }, 800);
